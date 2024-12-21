9f3f70c250178b91a285c182d3cb1b5d1ba95d7f
nicodubi <nicodubi@hotmail.com>
2024-12-21 18:36:00 -0300

    Add logic to build complete poster URL from TMDB API

--- app/src/main/java/com/notflix/notflix/data/source/network/MoviesRemoteDataSourceImpl.kt
+++ app/src/main/java/com/notflix/notflix/data/source/network/MoviesRemoteDataSourceImpl.kt
@@ -10,9 +10,19 @@ class MoviesRemoteDataSourceImpl @Inject constructor(
     private val moviesNetworkServices: MoviesNetworkServices
 ) :
     MoviesRemoteDataSource {
+    private val BASE_IMAGE_TMDB_URL = "https://image.tmdb.org/t/p/w500/"
+
     override suspend fun getMoviesNetwork(): List<Movie> {
         val randomMoviesNames = listOf("Harry", "Spider", "Ring", "Batman", "IronMan")
         val moviesResponse = moviesNetworkServices.getMovies(randomMoviesNames.random())
-        return moviesResponse.results
+        val movies = buildURLPosterImages(moviesResponse.results)
+        return movies
     }
+
+    private fun buildURLPosterImages(movies: List<Movie>) =
+        movies.map { movie ->
+            val posterPath = movie.poster_path?.let { BASE_IMAGE_TMDB_URL + it }
+            movie.copy(poster_path = posterPath)
+        }
+
 }
