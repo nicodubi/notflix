db4104197e15c88f7423d646251cbd365c384e3e
nicodubi <nicodubi@hotmail.com>
2025-01-24 14:20:06 -0300

    Create Navigation logic with NavController,NavHost,Routes,Destinations

--- app/src/main/java/com/notflix/notflix/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/notflix/notflix/presentation/screens/MainActivity.kt
@@ -1,35 +1,65 @@
 package com.notflix.notflix.presentation.screens
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.notflix.notflix.data.repository.MoviesRepository
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.notflix.notflix.presentation.screens.moviedetail.MovieDetailScreen
+
 import com.notflix.notflix.presentation.screens.movieshome.MoviesHomeScreen
-import com.notflix.notflix.presentation.viewmodel.MoviesViewModel
 import com.notflix.notflix.ui.theme.NotflixTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @kotlinx.serialization.Serializable
+    object MoviesHomeScreenRoute
+
+    @kotlinx.serialization.Serializable
+    data class MovieDetailScreenRoute(val movieTitle: String)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NotflixAppContainer {
-                MoviesHomeScreen()
+                NotflixAppNavigation()
             }
         }
+    }
+}
+
+@Composable
+fun NotflixAppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = MainActivity.MoviesHomeScreenRoute) {
+        composable<MainActivity.MoviesHomeScreenRoute> {
+            MoviesHomeScreen(onNavigateToMovieDetail = {
+                navController.navigate(route = MainActivity.MovieDetailScreenRoute(movieTitle = it.title))
+            })
+        }
+
+        composable<MainActivity.MovieDetailScreenRoute> {backStackEntry ->
+            val movieDetailScreen : MainActivity.MovieDetailScreenRoute = backStackEntry.toRoute()
+            MovieDetailScreen(movieDetailScreen.movieTitle)
+        }
+
     }
 }
 
