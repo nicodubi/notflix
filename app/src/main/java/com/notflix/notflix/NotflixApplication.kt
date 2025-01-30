0f44350ad5769e94da9fc68c19594d344c956a98
nicodubi <nicodubi@hotmail.com>
2025-01-30 19:13:24 -0300

    Add Timber logging to project

--- app/src/main/java/com/notflix/notflix/NotflixApplication.kt
+++ app/src/main/java/com/notflix/notflix/NotflixApplication.kt
@@ -2,10 +2,19 @@ package com.notflix.notflix
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 /**
  * Created by Nicolas Dubiansky on 10/12/2024.
  */
 @HiltAndroidApp
 class NotflixApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
 }
