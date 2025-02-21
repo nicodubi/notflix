f7bb714765717b69d0d50873f29b82aad2e8b2ed
nicodubi <nicodubi@hotmail.com>
2025-02-21 13:19:50 -0300

    Refactor NavGraph Composable initial fun

--- app/src/main/java/com/notflix/notflix/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/notflix/notflix/presentation/screens/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -39,16 +35,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NotflixAppContainer {
-                NotflixAppNavigation()
+            NotflixTheme {
+                NotflixAppNavGraph()
             }
         }
     }
 }
 
 @Composable
-fun NotflixAppNavigation() {
+fun NotflixAppNavGraph() {
     val navController = rememberNavController()
+    val scope = rememberCoroutineScope()
+
     NavHost(navController = navController, startDestination = MainActivity.MoviesHomeScreenRoute) {
         composable<MainActivity.MoviesHomeScreenRoute> {
             MoviesHomeScreen(onNavigateToMovieDetail = {
@@ -65,16 +63,4 @@ fun NotflixAppNavigation() {
 }
 
 
-@Composable
-fun NotflixAppContainer(content: @Composable () -> Unit) {
-    NotflixTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Surface(
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                content()
-            }
-        }
-    }
-}
 
